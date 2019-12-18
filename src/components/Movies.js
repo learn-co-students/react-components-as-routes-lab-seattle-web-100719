2ae67e8047df8f44b94b8a8dc40985ab8114e3fb
Matt Gahrns <matthewgahrns@hotmail.com>
2019-12-17 17:05:35 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const renderMovies = () => {
+    return movies.map(movie => {
+      return (
+      <div>
+        {movie.title}
+        {movie.time}
+        <ul>{movie.genres}</ul>
+      </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovies()}
     </div>
   );
 };
