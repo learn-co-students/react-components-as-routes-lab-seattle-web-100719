aedc7772b91a1b7b99e5850b8dd5a18f8d0c423b
Taljjaa <jenken131@gmail.com>
2019-12-17 16:17:33 -0800

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { actors } from '../data';
+import React from "react";
+import { actors } from "../data";
 
 const Actors = () => {
+  const renderActors = () => {
+    return actors.map(actor => {
+      return (
+        <div>
+          <p>{actor.name}</p>
+          <ul>
+            {actor.movies.map(movie => {
+              return <li>{movie}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    });
+  };
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {renderActors()}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
+  const renderMovies = () => {
+    return movies.map(movie => {
+      return (
+        <div>
+          <p>{movie.title}</p>
+          <p>{movie.time}</p>
+          <ul>
+            {movie.genres.map(genre => {
+              return <li>{genre}</li>;
+            })}
+          </ul>
+        </div>
+      );
+    });
+  };
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {renderMovies()}
     </div>
   );
 };
