5ebd30c65ce8c7e7ab4f4a04642b9e6aa5cb76b2
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2019-12-17 21:48:37 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const renderMovieList = () => {
+    return movies.map(movie => {
+      return <div className="actor"> {movie.title}: {movie.time} {movie.genres} {movie.metaScore} </div>
+    })
+  }
+  
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {renderMovieList()}
     </div>
   );
 };
