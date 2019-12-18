10034c3d3a2de0c8ec1ab134f8803c0f82c31cf6
Soumya_Goyal <somyagoyal739@gmail.com>
2019-12-18 09:57:49 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,18 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {
+        movies.map(movie => {
+          return (
+            <div>
+              {movie.title}
+              {movie.time}
+              {movie.genres.map(genre => <ul>{genre}</ul>)}
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route
 } from 'react-router-dom';
-import NavBar from '../components/NavBar';
+import Navbar from '../components/NavBar'
 import Home from '../components/Home';
 import Actors from '../components/Actors';
 import Directors from '../components/Directors';
@@ -12,9 +12,18 @@ import Movies from '../components/Movies';
 
 const App = (props) => {
   return (
-    <Router>
-      {/*{code here}*/}
-    </Router>
+    <div>
+      <Navbar />
+      <Router>
+        <div>
+
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" component={Movies} />
+          <Route exact path="/directors" component={Directors} />
+          <Route exact path="/actors" component={Actors} />
+        </div>
+      </Router>
+    </div>
   );
 };
 
