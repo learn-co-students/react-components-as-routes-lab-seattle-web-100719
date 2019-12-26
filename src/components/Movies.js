c8435fa468a7277c746cffc22cfa582c18221df5
alisabaj <alisabaj@gmail.com>
2019-12-26 11:30:17 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
+
+const eachMovie = () => {
+  return movies.map((movie, index) => {
+    return (
+      <div key={index}>
+        <h2>{movie.title}</h2>
+        <h3>{movie.time}</h3>
+        <ul>
+          {movie.genres.map(genre => {
+            <li>{genre}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  });
+};
 
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {/* {eachMovie()} */}
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h2>Movie: {movie.title}</h2>
+          <h3>Run time: {movie.time}</h3>
+          <h3>Genres:</h3>
+          <ul>
+            {movie.genres.map((genre, index) => (
+              <li key={index}>{genre}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
